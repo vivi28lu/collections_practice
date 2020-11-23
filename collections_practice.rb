aa1e604cab53d21f5799b21383c0d3142e3e079d
viviana <vivi28lu@gmail.com>
2020-11-22 22:20:37 -0500

    commit tree 04cf29ebc07ee48ef2c1542c2b4ca342c8650061

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
+num = [2, 8, 0, 4 ,1 , 5]
+def sort_array_asc(num)
+    num.sort do |a, b|
+    a <=> b
+    end
+end
 
+def sort_array_desc(num)
+    num.sort.reverse do |b, a|
+        b <=> a 
+    end
+end
+
+def sort_array_char_count(animals)
+    animals.sort.chr do |a, b|
+        a <=> b
+    end
+end
